23bfbe873673ba599085274e4ac076ecb2f0e8a6
לילך אברהם <lilachavr123@gmail.com>
2020-06-23 15:38:33 +0300

    db url

--- src/datebase.js
+++ src/datebase.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://lilach123:lilach123@nahairstyle-wem4a.mongodb.net/<dbname>?retryWrites=true&w=majority",
+mongoose.connect(process.env.DB_URL,
  { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }
   ).then(db => console.log('Datebase is Connected!'))
-
