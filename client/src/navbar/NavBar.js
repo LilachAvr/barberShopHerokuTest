a4daff154e6737550241819ae0a5a3624dc73cd3
לילך אברהם <lilachavr123@gmail.com>
2020-07-14 13:52:37 +0300

    heruko navigation css

--- client/src/navbar/NavBar.js
+++ client/src/navbar/NavBar.js
@@ -91,25 +91,26 @@ class NavBar extends Component {
         //     </Navbar.Collapse>
         // </Navbar>
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#">
+                <img src='/logo.png' alt='logo' id='logoNaftali'/>
+                </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link to="/">בית</Nav.Link>
+                    <Nav.Link to='/Gallery'>גלריה</Nav.Link>
+                    <Nav.Link to='/PriceList'>מחירון</Nav.Link>
+                    <Nav.Link to='/Products'>המוצרים שלנו</Nav.Link>
                     <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <Nav.Link href="#featureggs">ttttt</Nav.Link>
+                        <Nav.Link href="#pricifdgng">Prittttcing</Nav.Link>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#deets">More deets</Nav.Link>
-                    <Nav.Link eventKey={2} href="#memes">
-                        Dank memes
-  </Nav.Link>
+                    <Nav.Link href="#deets">שלום ,אורח!</Nav.Link>
+                    <Nav.Link eventKey={2} to="/SignUp"> התחברות </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
